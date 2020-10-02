82ebac7fc832ec162f9988df2b90bbaa57ed0588
rola-gh <rolla.ghanima@gmail.com>
2020-10-02 23:51:49 +0300

    push

---
+++ app/js/script.js
@@ -0,0 +1,43 @@
+
+//
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         if (this.scrollY > 70)
+//             $('nav').addClass("sticky");
+//         else
+//             $('nav').removeClass("sticky");
+//
+//     });
+// },
+
+
+$(function() {
+    $(".toggle").on("click", function() {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+        } else {
+            $(".item").addClass("active");
+        }
+    });
+});
+
+//
+function isSubmitted() {
+    alert("شكراً لتواصلك معنا");
+}
+
+//slider
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
