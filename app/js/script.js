9ae5e7e26fd3ef3d84c0985531aa1fe508f317f2
rola-gh <rolla.ghanima@gmail.com>
2020-10-03 05:01:09 +0300

    responsive

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,4 @@
 
-//
-// $(document).ready(function () {
-//     $(window).scroll(function () {
-//         if (this.scrollY > 70)
-//             $('nav').addClass("sticky");
-//         else
-//             $('nav').removeClass("sticky");
-//
-//     });
-// },
-
-
 $(function() {
     $(".toggle").on("click", function() {
         if ($(".item").hasClass("active")) {
